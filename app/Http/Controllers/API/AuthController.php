af1d110321acc7d037536f6d92c231c646beb385
Yakov Gorbunov <avatargfa@gmail.com>
2023-11-13 14:06:44 +0800

    Add authentication via Laravel Passport

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        if (auth()->attempt($credentials)) {
+            $user = Auth::user();
+            $user['token'] = $user->createToken('authToken')->accessToken;
+            return response()->json([
+                'user' => $user
+            ], 200);
+        }
+        return response()->json([
+            'message' => 'Invalid credentials'
+        ], 402);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'User created successfully',
+            'user' => $user,
+            'token' => $user->createToken('authToken')->accessToken
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::user()->tokens()->delete();
+        return response()->json([
+            'message' => 'Successfully logged out',
+        ]);
+    }
+
+}
