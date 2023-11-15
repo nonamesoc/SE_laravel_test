c2437edff3e68ad6830f1cbfbd91df68de9762c2
Yakov Gorbunov <avatargfa@gmail.com>
2023-11-15 18:04:50 +0800

    Update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,11 +3,15 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Note;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
+
     /**
      * Seed the application's database.
      */
@@ -15,11 +19,34 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-         \App\Models\User::factory()->create([
-             'name' => 'admin',
-             'email' => 'admin@example.com',
-             'password' => Hash::make('123456'),
-             'role' => 'admin'
-         ]);
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('123456'),
+            'role' => 'admin',
+        ]);
+
+        $user1 = User::factory()->create([
+            'name' => 'user1',
+            'email' => 'user1@example.com',
+            'password' => Hash::make('123456'),
+        ]);
+
+        $user2 = User::factory()->create([
+            'name' => 'user2',
+            'email' => 'user2@example.com',
+            'password' => Hash::make('123456'),
+        ]);
+
+        Note::factory()
+            ->count(6)
+            ->state(new Sequence(
+                ['user_id' => $user1->id],
+                ['user_id' => $user2->id],
+            ))
+            ->create([
+                'text' => fake()->text()
+            ]);
     }
+
 }
